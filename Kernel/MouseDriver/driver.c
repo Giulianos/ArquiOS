63b34b15612e5f0e01155ecf0aa30dc2eff460b9
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2017-06-02 00:57:24 -0300

    Added comments

--- Kernel/MouseDriver/driver.c
+++ Kernel/MouseDriver/driver.c
@@ -11,31 +11,43 @@ tomando informacion de:
 
 #define MOUSE_WAIT_READ   0x00
 #define MOUSE_WAIT_WRITE  0x01
+#define MOUSE_WAIT_TIMEOUT 10000 //Cantidad de iteraciones antes de abortar espera
+
+static uint16_t mousePositionX = 0;
+static uint16_t mousePositionY = 0;
 
 static void mouseWait(uint8_t bit);
 static uint8_t mouseRead();
 static void mouseWrite(uint8_t data);
 
+//Comandos del mouse: http://wiki.osdev.org/Mouse_Input#Useful_Mouse_Command_Set
+
+void mouseDriver()
+{
+
+}
+
 void initMouse()
 {
-  //Habilito aux mouse dev.
+  //Habilito aux mouse dev. (opcional, pero mejor tenerlo)
   mouseWait(MOUSE_WAIT_WRITE); //Espero a poder escribir
   outputB(0x64, 0xA8);
   //Habilito el aux port
   mouseWait(MOUSE_WAIT_WRITE);
   outputB(0x64, 0x20); //Pido el compaq status byte
-  mouseWait(MOUSE_WAIT_READ); //Espero los datos
-  uint8_t compaq_status = inputB(0x60); //Leo el compaq status byte
+  uint8_t compaq_status = mouseRead(); //Leo el compaq status byte
   compaq_status |= 0x02; //Set bit 1 del compaq status byte
-  //compaq_status &= 0xDF; //Clear bit 5 del compaq status byte
   mouseWait(MOUSE_WAIT_WRITE);
   outputB(0x64, 0x60); //Indico que voy a enviar el nuevo compaq status byte
   mouseWait(MOUSE_WAIT_WRITE); //Espero a poder escribir
   outputB(0x60, compaq_status);
 
+  /* Default conf:
+  Disables streaming, sets the packetrate to 100 per second, and resolution to 4 pixels per mm. */
   mouseWrite(0xF6);
   mouseRead();  //Espero el ACK
 
+  /* Activo el envio automatico de paquetes */
   mouseWrite(0xF4);
   mouseRead();  //Espero el ACK
 
@@ -43,27 +55,34 @@ void initMouse()
 
 static void mouseWrite(uint8_t data)
 {
+  //Espero a poder escribir
   mouseWait(MOUSE_WAIT_WRITE);
+  //Le digo al mouse que le voy a enviar un comando
   outputB(0x64, 0xD4);
+  //Espero a poder escribir
   mouseWait(MOUSE_WAIT_WRITE);
+  //Le envio el comando
   outputB(0x60, data);
 }
 
 static uint8_t mouseRead()
 {
+  //Espero a poder leer
   mouseWait(MOUSE_WAIT_READ);
+  //Leo del mouse
   return inputB(0x60);
 }
 
 /* Espera al bit de lectura:0 | escritura:1 */
 static void mouseWait(uint8_t bit)
 {
+  uint16_t timeout = MOUSE_WAIT_TIMEOUT;
   if(bit>1)
     return;
   if(!bit)
   {
-    while(!(inputB(0x64)&0x01)){}
+    while(!(inputB(0x64)&0x01) || timeout){ timeout--; }
     return;
   }
-  while(inputB(0x64)&0x02){}
+  while(inputB(0x64)&0x02 || timeout){ timeout--; }
 }
