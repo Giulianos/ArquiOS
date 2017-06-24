8f8631b7f2310a7e24547b4c0604eb87fe104b5b
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2017-06-24 17:28:53 -0300

    Work on module loading

--- Kernel/ModulesManager/moduleman.c
+++ Kernel/ModulesManager/moduleman.c
@@ -3,14 +3,53 @@ static module_t modules[256];
 extern uint8_t endOfKernelBinary;
 extern uint8_t endOfKernel;
 
+/*
+*      +--------------------+
+*      |       KERNEL       |
+*      | MODULES/KERNEL BSS | 0x0000000 - endOfKernel
+*      |....................|
+*      +--------------------+
+*      |   COPIED MODULES   | 0x0A00000 - 0x13FFFFF
+*      +--------------------+
+*      |    RUNTIME PAGE    | 0x1400000 - 0x15FFFFF
+*      +--------------------+
+*
+*/
+
+static uint8_t * expandedModulesArea = (uint8_t *)0xA00000; //10MB
+
 /* Loads modules  */
-void loadModulesToKernel(module_t * modules, uint8_t quantity)
+static void loadModulesToKernel(module_t * modules, uint8_t quantity)
 {
   //Read how many modules were packed
-  endOfKernelBinary
+  uint32_t modulesQuantity = *((uint32_t *)(&endOfKernelBinary));
+  uint8_t * currentModule = (uint8_t *)(&endOfKernelBinary+sizeof(uint32_t));
+  uint8_t * moduleDest = expandedModulesArea;
+  uint8_t i;
+  for(i=0; i<modulesQuantity; i++)
+  {
+    loadModuleToKernel(&currentModule, &moduleDest, i);
+  }
 }
 
-void loadModuleToRun(uint8_t id)
+static void loadModuleToKernel(uint8_t ** module, uint8_t ** targetModuleAddress, uint8_t moduleNum)
+{
+	uint32_t moduleSize = readUint32(module);
+  modules[moduleNum].id = moduleNum;
+  modules[moduleNum].dir = *targetModuleAddress;
+	memcpy(targetModuleAddress, *module, moduleSize);
+	*module += moduleSize;
+  *targetModuleAddress += moduleSize;
+}
+
+static void loadModuleToRun(uint8_t id)
 {
 
+}
+
+static uint32_t readUint32(uint8_t ** address)
+{
+	uint32_t result = *(uint32_t*)(*address);
+	*address += sizeof(uint32_t);
+	return result;
 }
